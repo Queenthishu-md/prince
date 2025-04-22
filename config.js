b23739e4943b25acc29aba8c27f7240bfe81e605
Queenthishu-md <queenthishu@gmail.com>
2025-04-22 14:45:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PRINCE-MD=OoBxyKhL#yjMD305kgNlDo5fRot8_sSFqFuBbMoeGUpnBXUEg7xg",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
